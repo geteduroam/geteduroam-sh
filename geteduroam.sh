5f0e166425a05e5790d1c58d4d5ff7011f8683e9
Jørn Åne de Jong <jorn.dejong@uninett.no>
2020-10-24 01:23:42 +0200

    Use [ instead of test

--- geteduroam.sh
+++ geteduroam.sh
@@ -6,7 +6,7 @@
 # LICENSE: BSD-3-Clause
 # http://opensource.org/licenses/BSD-3-Clause
 
-if test -z $1
+if [ -z $1 ]
 then
 	printf '\033[0musage: %s base-url [realm]\n\nexample: \033[1m%s "http://[::1]:1080" demo.eduroam.no >eduroam.eap-config\033[0m\n\nPlease note: base-url must be available from both your webbrowser and this script,\nuse -R1080:localhost:1080 if you want to test a local server from remote\n\n' "$0" "$0" >&2
 	exit 2
@@ -18,18 +18,18 @@ PORT="0$3"
 while [ $PORT -lt 1024 -o $PORT -gt 65535 ]
 do
 	PORT=$RANDOM
-	test -z "$PORT" && PORT=$(tr -cd '0123456789' </dev/urandom | head -c5)
+	[ -z "$PORT" ] && PORT=$(tr -cd '0123456789' </dev/urandom | head -c5)
 done
 PORT=$(echo $PORT | sed -es/\^0\*//)
 CLIENT_ID="app.geteduroam.sh"
 REDIRECT_URI="http://127.0.0.1:$PORT/"
 
-if test -n "$2"
+if [ -n "$2" ]
 then
 	REALM_PARAM="?realm=$2"
 fi
-test "$(echo "$URL" | head -c8)" == 'https://' \
-	|| test "$(echo "$URL" | head -c8)" == 'http://' \
+[ "$(printf "%s" "$URL" | head -c8)" == 'https://' ] \
+	|| [ "$(printf "%s" "$URL" | head -c7)" == 'http://' ] \
 	|| URL="https://$URL"
 
 AUTHORIZE_URL="$URL/oauth/authorize/$REALM_PARAM"
@@ -114,9 +114,9 @@ window() { # $1 = title, $2 = text
 
 set -e
 
-test -f "$REFRESH_TOKEN_FILENAME" && refresh_token="$(cat "$REFRESH_TOKEN_FILENAME")"
+[ -f "$REFRESH_TOKEN_FILENAME" ] && refresh_token="$(cat "$REFRESH_TOKEN_FILENAME")"
 
-if test -n "$refresh_token"
+if [ -n "$refresh_token" ]
 then
 	token_data="$(curl \
 		--fail \
@@ -133,7 +133,7 @@ then
 fi
 
 printf '\n\n\n\n\n' >&2
-if test -z "$access_token"
+if [ -z "$access_token" ]
 then
 	code_challenge="$(printf "$CODE_VERIFIER" | sha256bin | urlb64)"
 	separator=$(echo "$AUTHORIZE_URL" | grep -Fq '?' && printf '&' || printf '?')
@@ -175,7 +175,7 @@ curl --fail --silent --show-error -HAuthorization:"Bearer $access_token" -d '' "
 printf '\n\n\n\n\n' >&2
 window 'Success' 'Successfully downloaded eap-config file'
 
-if test -n "$refresh_token"
+if [ -n "$refresh_token" ]
 then
 	echo "$refresh_token" > "$REFRESH_TOKEN_FILENAME"
 fi
