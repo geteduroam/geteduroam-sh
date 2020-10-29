6017a9a9b9ca1c27c6b6539b9b5a3ee242564efd
Jørn Åne de Jong <jorn.dejong@uninett.no>
2020-10-29 14:24:31 +0100

    Various small fixes

--- geteduroam.sh
+++ geteduroam.sh
@@ -23,7 +23,7 @@ do
 	PORT=$RANDOM
 	[ -z "$PORT" ] && PORT=$(tr -cd '0123456789' </dev/urandom | head -c5)
 done
-PORT=$(printf "%s" $PORT | sed -es/\^0\*//)
+PORT=$(printf %s $PORT | sed -es/\^0\*//)
 CLIENT_ID="app.geteduroam.sh"
 
 # nc may not be able to listen on IPv6,
@@ -34,14 +34,14 @@ if [ -n "$2" ]
 then
 	REALM_PARAM="?realm=$2"
 fi
-[ "$(printf "%s" "$URL" | head -c8)" == 'https://' ] \
-	|| [ "$(printf "%s" "$URL" | head -c7)" == 'http://' ] \
+[ "$(printf %s "$URL" | head -c8)" == 'https://' ] \
+	|| [ "$(printf %s "$URL" | head -c7)" == 'http://' ] \
 	|| URL="https://$URL"
 
 AUTHORIZE_URL="$URL/oauth/authorize/$REALM_PARAM"
 TOKEN_URL="$URL/oauth/token/$REALM_PARAM"
 GENERATE_URL="$URL/api/eap-config/$REALM_PARAM"
-REFRESH_TOKEN_FILENAME=".geteduroam-refresh-$(printf "%s" "$TOKEN_URL" | openssl sha256 | tail -c16)"
+REFRESH_TOKEN_FILENAME=".geteduroam-refresh-$(printf %s "$TOKEN_URL" | openssl sha256 | tail -c16)"
 
 CODE_VERIFIER="$(LC_ALL=C tr -cd '[:alnum:]-_.~' </dev/urandom | head -c128)"
 STATE="$(LC_ALL=C tr -cd '[:alnum:]_-' </dev/urandom | head -c43)"
@@ -67,10 +67,10 @@ getQuery() { # $1 = key
 	tr \& \\n | grep --fixed-strings "${1}=" | head -n1 | cut -d= -f2-
 }
 getJson() { # $1 = key
-	if jq --version 2>&1 >/dev/null
+	if jq --version 2>/dev/null >&2
 	then
 		jq ".$1" | jq --raw-output 'select(type == "string")'
-	elif json_pp -v 2>&1 >/dev/null
+	elif json_pp -v 2>/dev/null >&2
 	then
 		# This is not a reliable JSON parser, we still get JSON escaped strings,
 		# and multiline would fail horribly.
@@ -91,7 +91,7 @@ fifo() {
 	fi
 	cat fifo
 }
-trap "rm fifo" EXIT
+trap "rm -f fifo" EXIT
 
 serve() {
 	[ -p fifo ] || mkfifo fifo
@@ -107,7 +107,7 @@ serve() {
 		# so we have to convince the browser to close the TCP connection
 		# Only the Linux version of nc seems to be able to get stuck
 
-		error="$(printf "%s" "$line" | urlToQuery | getQuery error)"
+		error="$(printf %s "$line" | urlToQuery | getQuery error)"
 		if [ -n "$error" ]
 		then
 			if [ "$error" = 'access_denied' ]
@@ -119,7 +119,7 @@ serve() {
 				window 'Error' "$(printf '\033[31mAn unexpected error occurred: \033[1m%s\033[0;31m, press ^C to exit' "$error")"
 			fi
 		else
-			printf "%s" "$line" | urlToQuery
+			printf %s "$line" | urlToQuery
 			# We lie about the Content-Length (we report two bytes less, the CRLF)
 			# so the browser will hopefully disconnect, this will speed up the process
 			# because nc will terminate quicker
@@ -131,7 +131,7 @@ serve() {
 
 redirect() { # $1 = url
 	fifo | listen $PORT | while read line
-	len=$(printf "%s" "$1" | wc -c)
+	len=$(printf %s "$1" | wc -c)
 	do
 		# We lie about the Content-Length (we report two bytes less, the CRLF)
 		# so the browser will hopefully disconnect, this will speed up the process
@@ -149,7 +149,7 @@ set -e
 
 [ -f "$REFRESH_TOKEN_FILENAME" ] && refresh_token="$(cat "$REFRESH_TOKEN_FILENAME")"
 
-if [ "$(echo '{"foo":"test"}' | getJson foo)" != 'test' ]
+if [ "$(printf '{"foo":"test"}' | getJson foo)" != 'test' ]
 then
 	printf '\033[1;41;37m Missing JSON parser   \033[0m\n\033[47;30m Tried \033[34mjq\033[30m and \033[34mjson_pp\033[30m. \033[0m\n\n'
 	exit 2
@@ -167,15 +167,15 @@ then
 		--data-urlencode grant_type=refresh_token \
 		--data-urlencode "refresh_token=$refresh_token" \
 		"$TOKEN_URL")" || true
-	access_token=$(printf "%s" "$token_data" | getJson access_token)
-	refresh_token=$(printf "%s" "$token_data" | getJson refresh_token)
+	access_token=$(printf %s "$token_data" | getJson access_token)
+	refresh_token=$(printf %s "$token_data" | getJson refresh_token)
 fi
 
 printf '\n\n\n\n\n' >&2
 if [ -z "$access_token" ]
 then
 	code_challenge="$(printf "$CODE_VERIFIER" | sha256bin | urlb64)"
-	separator=$(printf "%s" "$AUTHORIZE_URL" | grep --fixed-strings --quiet '?' && printf '&' || printf '?')
+	separator=$(printf %s "$AUTHORIZE_URL" | grep --fixed-strings --quiet '?' && printf '&' || printf '?')
 	authorize_url="${AUTHORIZE_URL}${separator}response_type=code&code_challenge_method=S256&scope=$SCOPE&code_challenge=$code_challenge&redirect_uri=$REDIRECT_URI&client_id=$CLIENT_ID&state=$STATE"
 	[ -n "$BROWSER" ] && "$BROWSER" "$REDIRECT_URI" || \
 		window 'Please visit the following URL in your webbrowser' "$(printf "\033[4;34m$REDIRECT_URI")"
@@ -186,8 +186,8 @@ then
 	do
 		response="$(serve)"
 		window 'Please wait' 'Parsing response'
-		code="$(printf "%s" "$response" | getQuery code)"
-		state="$(printf "%s" "$response" | getQuery state)"
+		code="$(printf %s "$response" | getQuery code)"
+		state="$(printf %s "$response" | getQuery state)"
 	done
 
 	# We have received an access token, so we must assume our refresh_token is burned (if we used one)
@@ -205,8 +205,8 @@ then
 		--data-urlencode "code=$code" \
 		--data-urlencode "code_verifier=$CODE_VERIFIER" \
 		"$TOKEN_URL")"
-	access_token=$(printf "%s" "$token_data" | getJson access_token)
-	refresh_token=$(printf "%s" "$token_data" | getJson refresh_token)
+	access_token=$(printf %s "$token_data" | getJson access_token)
+	refresh_token=$(printf %s "$token_data" | getJson refresh_token)
 fi
 
 window 'Please wait' "Fetching eap-config from $GENERATE_URL"
@@ -217,5 +217,5 @@ window 'Success' 'Successfully downloaded eap-config file'
 
 if [ -n "$refresh_token" ]
 then
-	printf "%s" "$refresh_token" > "$REFRESH_TOKEN_FILENAME"
+	printf %s "$refresh_token" > "$REFRESH_TOKEN_FILENAME"
 fi
